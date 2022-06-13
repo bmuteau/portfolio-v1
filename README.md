2bcef56e874711ddf0d35f02df02da942f1ddcdb
Brandon Muteau <muteaub@gmail.com>
2022-06-13 15:19:57 +0200

    skills page mobile front done

--- README.md
+++ README.md
@@ -5,8 +5,17 @@ ToDo :
         * Link all page with href | Home page 
         * locker open on click | Home page Mobile
         * animation grey to click in a logo | Home page
+        * think to a cross button for return in home page
     
     Back-End :
         * loop for project | Project page
         * search | Project page 
+        * loop for stack | ProjectProfil page
+        * loop for stackList | Skill page
+
+
+    Back-Office : 
+        * Make the back office for push :
+            - stack item
+            - project info 
         
---
+++ view/skill.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="../style/styles.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
+
+    <title>Brandon Muteau - Mes compétences</title>
+</head>
+
+<body class="white">
+
+    <div class="datetime">
+        <p 
+            id="time"
+            style=" 
+                color: black;
+                margin-top: -24px;
+                margin-left: -265px;
+                font-size: 25px;
+            ">
+        </p>
+    </div>
+
+    <h1 class="name">Mes compétences</h1>
+
+    <form method="">
+        <button class="search-button" type='submit'><span class='icone-loupe'><img src="../src/icon/find_loupe.svg"></span></button>
+        <input type="text" name="search" placeholder="Rechercher une compétences" class="search">
+    </form>
+
+    <div id="circle"><p class="circleText">BM</p></div>
+    <div class="info">
+        <p class="staticName">Brandon Muteau</p>
+        <p class="mySkills">Mes stacks maitrisés</p>
+    </div>
+
+<hr>
+   
+    <ul class="stackList"> <h2>Mes stacks: </h2>
+        <li class="stackItem">
+             <img src="../src/icon/badge_html.svg" class="imgStack" alt="html">Nom de la stack
+        </li>
+    </ul>
+
+</body>
+
+<script src="../script/datetime.js"></script>
+
+
+</html>
