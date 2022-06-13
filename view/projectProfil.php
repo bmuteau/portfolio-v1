edccac6b5da956d5b1b44499a577adeaa8b4b22e
Brandon Muteau <muteaub@gmail.com>
2022-06-13 14:16:38 +0200

    projectProfil page mobile done

---
+++ view/projectProfil.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="../style/styles.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
+
+    <title>Brandon Muteau - Nom du projet</title>
+</head>
+
+<body class="white">
+
+    <div class="datetime">
+        <p 
+            id="time"
+            style=" 
+                color: black;
+                margin-top: -24px;
+                margin-left: -265px;
+                font-size: 25px;
+            ">
+        </p>
+    </div>
+
+    <h1 class="name">Nom du projet</h1>
+
+    <img src="../src/icon/drive.svg" class="projectImage" alt="drive">
+    <div class="infoProject">
+        <p class="projectName"> Nom du projet</p>
+        <p class="projectType"> Pro ou perso </p>
+        <p class="ProjectDuration"> Temps de dévelopement</p>
+        <p class="ProjectLink"><a href="" class="link"> Lien du projet </a></p>
+    </div>
+
+    <h2 class="desc">Description</h2>
+    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut incidunt omnis dolores delectus perspiciatis odio error iure ad vero impedit.</p>
+
+    <h2 class="desc">Stack utilisés</h2>
+
+    <div class="stack">
+        <div class="stack1">
+            <img src="../src/icon/badge_html.svg" alt="html">
+            <p class="texte">Nom </p>
+        </div>
+    </div>
+
+</body>
+
+<script src="../script/datetime.js"></script>
+
+
+</html>
